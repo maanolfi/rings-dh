6be4e23f5c05b6ec3f606b37638e7f5ab4423802
Marco Antonio <marco.oliveira92@hotmail.com>
2020-03-24 10:39:02 -0300

    tela-login-material-ui

--- front-end/src/pages/LoginR/index.js
+++ front-end/src/pages/LoginR/index.js
@@ -1,31 +1,133 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
 
-import { Container, Wrapper, Content, Video } from './styles';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import { CardMedia } from '@material-ui/core';
+
 import video from '../../images/videologin.mp4';
-import iconlogin from '../../images/iconlogin.svg';
+import logo from '../../images/logo.png';
 
-export default function LoginR() {
+function Copyright() {
   return (
-    <Container>
-      <Wrapper>
-        <h1>RINGS</h1>
-        <form>
-          <label htmlFor="name">Nome</label>
-          <input type="text" id="name" />
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
-          <label htmlFor="password">Senha</label>
-          <input type="password" id="password" />
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light'
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(2, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    width: '50px',
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
-          <button>Entrar / Cadastrar</button>
-        </form>
-        <img src={iconlogin} alt="Kiwi standing on oval" />
-      </Wrapper>
+export default function SignInSide() {
+  const classes = useStyles();
 
-      <Content>
-        <video autoPlay="true" muted loop>
-          <source src={video} type="video/mp4" />
-        </video>
-      </Content>
-    </Container>
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image}>
+        <CardMedia component="video" src={video} autoplay_="autoplay_" />
+      </Grid>
+
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <img src={logo} width="150px" alt="Rings" />
+
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Don't have an account? Sign Up
+                </Link>
+              </Grid>
+            </Grid>
+            <Box mt={5}>
+              <Copyright />
+            </Box>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
   );
 }
